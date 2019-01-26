67e597d33eca02d46258a707684fae9921ca1867
Dario <medariox@users.noreply.github.com>
2019-01-26 13:45:58 +0100

    Add core

--- revisum/review.py
+++ revisum/review.py
@@ -1,16 +1,7 @@
 
 import requests
-from github import Github
-from unidiff import PatchSet
 from snippet import Snippet
-from trainer import SnippetsTrainer
-
-from gensim.models.doc2vec import Doc2Vec
-
-g = Github("medariox", "comliebt92")
-
-repo = g.get_repo("requests/requests")
-pulls = repo.get_pulls(state='all')
+from unidiff import PatchSet
 
 
 class WeightedReview(object):
@@ -53,7 +44,7 @@ class WeightedReview(object):
     @property
     def patch_content(self):
         """
-        Gets the content of the pull request patch.
+        Get the content of the pull request patch.
 
         :return: The content of the patch.
         :type: str
@@ -67,46 +58,9 @@ class WeightedReview(object):
     @patch_content.setter
     def patch_content(self, content):
         """
-        Sets the content of the pull request patch.
+        Set the content of the pull request patch.
 
         :param content: The content of the patch.
         :type: str
         """
         self._patch_content = content
-
-
-review_count = 0
-snippets = []
-for pull in pulls:
-    reviews = pull.get_reviews()
-    rev_len = reviews.totalCount
-
-    # No reviews found
-    if rev_len == 0:
-        print("No reviews for: {0}".format(pull.title))
-        continue
-
-    print("Found reviews for: {0}".format(pull.title))
-    weighted_review = WeightedReview(repo.id, pull)
-    snippets += weighted_review.snippets()
-
-    review_count += 1
-    if review_count == 10:
-        break
-
-SnippetsTrainer(snippets).train()
-
-
-def eval_model():
-    model = Doc2Vec.load("d2v.model")
-
-    tokens = "param_ordered_dict = collections.OrderedDict((('z', 1), ('a', 1), ('k', 1), ('d', 1)))".split()
-    new_vector = model.infer_vector(tokens)
-    sims = model.docvecs.most_similar([new_vector])
-
-    print(sims)
-    print('--------------------------------------')
-    print('For {input} matched {result}!'.format(input=tokens, result=sims[0][0]))
-
-
-eval_model()
