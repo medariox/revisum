fa84f8f30e243ed7f8ca22652f650026e4af9a41
Dario <medariox@users.noreply.github.com>
2020-02-09 00:51:42 +0100

    Added collect API

--- revisum/api.py
+++ revisum/api.py
@@ -9,7 +9,7 @@ from revisum.chunk import Chunk
 
 
 @hug.get('/snippets/{snippet_id}')
-def retrieve(snippet_id: hug.types.text):
+def retrieve_snippet(snippet_id: hug.types.text):
     snippet = Snippet.load(snippet_id)
 
     if snippet:
@@ -34,8 +34,8 @@ def retrieve_chunk(chunk_id: hug.types.text):
 
 
 @hug.get('/repos/{repo_id}/snippets')
-def evaluate_code(repo_id: hug.types.number, snippet_url: hug.types.text,
-                  threshold: float = 0.75):
+def evaluate_snippet(repo_id: hug.types.number, snippet_url: hug.types.text,
+                     threshold: float = 0.75):
     snippet = SnippetCollector(repo_id).from_remote(snippet_url)
 
     return SnippetTrainer(repo_id, snippet).evaluate(
@@ -44,8 +44,8 @@ def evaluate_code(repo_id: hug.types.number, snippet_url: hug.types.text,
 
 
 @hug.get('/repos/{repo_id}/pulls/{pull_number}')
-def evaluate_snippet(repo_id: hug.types.number, pull_number: hug.types.number,
-                     threshold: float = 0.75):
+def evaluate_pull(repo_id: hug.types.number, pull_number: hug.types.number,
+                  threshold: float = 0.75):
     snippets = PullRequest(repo_id, pull_number).snippets
 
     return SnippetTrainer(repo_id, snippets).evaluate(
@@ -53,9 +53,16 @@ def evaluate_snippet(repo_id: hug.types.number, pull_number: hug.types.number,
     )
 
 
+@hug.put('/repos/{repo_id}/collect')
+def collect(repo_id: hug.types.number, limit: hug.types.number = 10):
+    SnippetCollector(repo_id).collect(limit=limit)
+
+    return 'Finished collecting for repository: {0}'.format(repo_id)
+
+
 @hug.put('/repos/{repo_id}/train')
 def train(repo_id: hug.types.number, limit: hug.types.number = 10, iterations: hug.types.number = 100):
     snippets = SnippetCollector(repo_id).from_pulls(limit=limit)
-
     SnippetTrainer(repo_id, snippets).train(iterations=iterations, force=True)
+
     return 'Finished training for repository: {0}'.format(repo_id)
